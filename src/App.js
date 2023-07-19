22c774cd033122b26eedfc8525de17188c810432
xuanbien12 <bien181221@gmail.com>
2023-07-19 20:20:45 +0700

    C

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import HomePage from './Pages/HomePage';
+import './assets/css/reset.css'
+import './assets/css/font.css'
+import './assets/css/commom.css'
+import AppProvider from './ProviderGlobal';
+function App() {
+  return (
+
+    <HomePage />
+  );
+}
+
+export default App;
